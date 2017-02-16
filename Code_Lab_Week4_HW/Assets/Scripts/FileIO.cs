c565e9d6cf55f7ed2b42528ad11aedf5da4aad79
GordonCoffeeCan <gordon.lee.6170@gmail.com>
2017-02-15 20:32:34 -0500

    backup

---
+++ Code_Lab_Week4_HW/Assets/Scripts/FileIO.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class FileIO : MonoBehaviour {
+
+    public string fileName = "testFile.txt";
+
+	// Use this for initialization
+	void Start () {
+        string finalFilePath = Application.dataPath + "/" + fileName;
+        StreamWriter sw = new StreamWriter(finalFilePath, true);
+
+        sw.WriteLine("this is a line");
+
+        sw.Close();
+
+        StreamReader sr = new StreamReader(finalFilePath);
+
+        while (!sr.EndOfStream) {
+            Debug.Log(sr.ReadLine());
+        }
+        sr.Close();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
